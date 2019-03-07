fd3a2baf7553d335fa556c8d0147d8854037663f
eiiizong <eiiizong@163.com>
2019-03-07 16:27:21 +0800

    增加less编译为css

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const favicon = require("serve-favicon");
-const lessMiddleware = require('less-middleware');
+const lessMiddleware = require("less-middleware");
 
 const path = require("path");
-const index = require('./routes/index.js');
+const index = require("./routes/index.js");
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// 配置
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // 路由
 app.use("/", index);
 // 使用 favicon.ico
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-// 使用public下的less文件
-app.use(lessMiddleware(path.join(__dirname, "public")));
+// 使用public下的less文件 html模板引用要写绝对路径 /css/index.html
+app.use(lessMiddleware(path.join(__dirname, "public"), { debug: true }));
 // 使用public下的静态文件
 app.use(express.static(path.join(__dirname, "public")));
-
 
 const port = 3002;
 app.listen(port, () => {
